7c4b0d3fc486e0221f87da4674f4f5c97375a0e0
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-20 13:56:02 -0600

    test: add tests for _DATA functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { _saveQuestion, _saveQuestionAnswer } from './utils/_DATA';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('_saveQuestion', () => {
+  it('will return an object containing formatted question', async () => {
+    const question = {
+      optionOneText: "work remotely?",
+      optionTwoText: "work in-office?",
+      author: "Bob Belcher",
+    }
+
+    const result = await _saveQuestion(question)
+    expect(result.id).toBeString()
+    expect(new Date(result.timestamp)).toBeDate()
+    expect(result.author).toEqual(question.author)
+    expect(result.optionOne.votes).toBeArray()
+    expect(result.optionOne.text).toEqual(question.optionOneText)
+    expect(result.optionTwo.votes).toBeArray()
+    expect(result.optionTwo.text).toEqual(question.optionTwoText)
+  })
+
+  it('will return an error if incorrect data is passed', async () => {
+    const question = {
+      optionOneText: "work remotely?",
+      optionTwoText: "work in-office?",
+    }
+
+    await expect(_saveQuestion(question)).rejects.toEqual("Please provide optionOneText, optionTwoText, and author")
+  })
+})
+
+describe('_saveQuestionAnswer', () => {
+  it('will return true', async () => {
+    const answer = {
+      authedUser: 'sarahedo',
+      qid: 'xj352vofupe1dqz9emx13r',
+      answer: 'optionOne',
+    }
+
+    const result = await _saveQuestionAnswer(answer)
+    expect(result).toEqual(true)
+  })
+
+  it('will return an error if incorrect data is passed', async () => {
+    const answer = {
+      authedUser: 'sarahedo',
+      qid: 'xj352vofupe1dqz9emx13r',
+    }
+
+    await expect(_saveQuestionAnswer(answer)).rejects.toEqual("Please provide authedUser, qid, and answer")
+  })
+})
