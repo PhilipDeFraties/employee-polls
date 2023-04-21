136315d3336d7180c7c60f3666e6feacba17acef
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-21 14:58:21 -0600

    feat: add Dashboard component
    sort question ids by those found in authedUser's questions, render Question components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,50 @@
+import { connect } from "react-redux";
+import Question from "./Question"; 
+
+const Dashboard = (props) => {
+  const { questionIds, authedUser, users } = props;
+
+  if (authedUser) {
+    const answeredQuestionIds = users[authedUser].questions;
+    const unansweredQuestionIds = questionIds.filter((id) => !answeredQuestionIds.includes(id));
+
+    return <div>
+      <h3 className="center">New Questions</h3>
+      <ul className="dashboard-list">
+        {
+          unansweredQuestionIds.map((id) => (
+            <li key={id}>
+              <Question id={id} />
+            </li>
+          ))
+        }
+      </ul>
+
+      <h3 className="center">Done</h3>
+      <ul className="dashboard-list">
+        {
+          answeredQuestionIds.map((id) => (
+            <li key={id}>
+              <Question id={id} />
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  }
+  return (
+    <h1>Dashboard</h1>
+  )
+};
+
+const mapStateToProps = ({ questions, authedUser, users }) => {
+  const questionIds = Object.keys(questions).sort((a, b) =>
+    questions[b].timestamp - questions[a].timestamp
+  )
+  return {
+    questionIds,
+    authedUser,
+    users
+  }
+}
+export default connect(mapStateToProps)(Dashboard);
