123072164c85736a97a5bd4b59d13c028485c476
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-22 16:11:33 -0600

    feat: add user answer action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,24 @@
 export const RECEIVE_USERS = "RECEIVE_USERS";
+export const ADD_ANSWER = "ADD_ANSWER";
 
 export function receiveUsers(users) {
   return {
     type: RECEIVE_USERS,
     users,
   };
+}
+
+function addUserAnswer(questionAnswer) {
+  return {
+    type: ADD_ANSWER,
+    questionAnswer
+  }
+}
+
+export const handleAddUserAnswer = (questionAnswer) => {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    dispatch(addUserAnswer({...questionAnswer, authedUser}))
+  }
 }
