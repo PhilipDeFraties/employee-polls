3d56de1ceda586f5d6f139c1e9690fa93198679f
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-21 18:26:16 -0600

    feat: add NewQuestion component

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { connect } from "react-redux";
+import { handleAddQuestion } from "../actions/questions";
+
+const NewQuestion = ({dispatch}) => {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const question = {
+      optionOne: inputs.optionOne,
+      optionTwo: inputs.optionTwo,
+    }
+    dispatch(handleAddQuestion(question))
+
+    setInputs({})
+  }
+
+  return (
+    <div>
+      <h3 className="center">Create Your Own Poll</h3>
+      <form onSubmit={handleSubmit} className="new-question">
+        {/* todo: redirect to / */}
+        <h4 className="center">First Option:</h4>
+          <input 
+            className="textarea"
+            type="text" 
+            name="optionOne"
+            value={inputs.optionOne || ""}
+            onChange={handleChange}
+            placeholder="Option One"
+          />
+        <h4 className="center">Second Option:</h4>
+          <input
+            className="textarea" 
+            type="text" 
+            name="optionTwo" 
+            value={inputs.optionTwo || ""} 
+            onChange={handleChange}
+            placeholder="Option Two"
+          />
+        <button className="btn" type="submit" disabled={!inputs.optionOne || !inputs.optionTwo}>
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default connect()(NewQuestion);
