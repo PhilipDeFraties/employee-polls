b09e2b110586e7c5b79a9a73c3bb2ea50f3077f1
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-21 20:27:29 -0600

    feat: add action logic for handling question answer

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,9 @@
-import { saveQuestion } from "../utils/api";
+import { saveQuestion, saveQuestionAnswer } from "../utils/api";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
 
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
 export const ADD_QUESTION = "ADD_QUESTION";
+export const ADD_QUESTION_ANSWER = "ADD_ANSWER";
 
 function addQuestion(question) {
   return {
@@ -24,6 +25,27 @@ export function handleAddQuestion(question) {
     }).then((question) => dispatch(addQuestion(question))).then(() => dispatch(hideLoading()))
   }
 }
+
+function addQuestionAnswer(questionAnswer) {
+  return {
+    type: ADD_QUESTION_ANSWER,
+    questionAnswer,
+  }
+}
+
+export function handleSaveQuestionAnswer(questionAnswer) {
+  return (dispatch, getState) => {
+    
+    dispatch(showLoading());
+    
+    return saveQuestionAnswer({
+      authedUser: questionAnswer.authedUser,
+      qid: questionAnswer.id,
+      answer: questionAnswer.answer,
+    }).then(() => dispatch(addQuestionAnswer(questionAnswer))).then(() => dispatch(hideLoading()))
+  }
+}
+
 
 export function receiveQuestions(questions) {
   return {
