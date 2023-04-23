c689376a47b71bc30cd0c87b7d219656a2c68a1a
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-23 14:02:03 -0600

    feat: update nav component to be connected
    connects with authedUser and users to return the id and avatarURL of the logged in user

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Nav = () => {
+const Nav = (props) => {
+  const { avatarURL, id } = props;
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/new">New Question</Link>
-        </li>
-      </ul>
-    </nav>
+    <div class="topnav">
+      <a>
+        <Link to="/">Home</Link>
+      </a>
+      <a>
+        <Link to="/new">New Question</Link>
+      </a>
+      <div class="topnav-right">
+        <b>
+          <img src={avatarURL} alt={`Avatar of ${id}`} className="avatar" />
+        </b>
+        <a>
+          <Link to="/logout">Logout</Link>
+        </a>
+      </div>
+    </div>
   )
 }
 
-export default Nav;
+const mapStateToProps = ({ authedUser, users }) => {
+  const { avatarURL, id } = users[authedUser]
+
+  return {
+    avatarURL,
+    id,
+  }
+}
+export default connect(mapStateToProps)(Nav);
