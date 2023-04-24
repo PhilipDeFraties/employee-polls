66c1193c05eeb638a9c0a62fb81b2d453ddeba0b
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-23 20:34:35 -0600

    feat: add action
    action reuses questions action const in order to chain dispatch from question creation

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USERS, ADD_ANSWER } from "../actions/users";
+import { ADD_QUESTION } from "../actions/questions";
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -18,7 +19,19 @@ export default function users(state = {}, action) {
             [qid]: answer,
           }
         }
-      }  
+      }
+    case ADD_QUESTION:
+      const { id, author } = action.question;
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          questions: [
+            ...state[author].questions,
+            id,
+          ]
+        }
+      }
     default:
       return state;
   };
