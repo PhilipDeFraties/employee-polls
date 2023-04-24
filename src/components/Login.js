dfac54c2d115f1c79518cd901e7b002c92ce1b27
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-23 20:31:28 -0600

    feat: update component logic
    adds useEffect to redirect unauthed user
    
    adds drop down select for choosing log in user

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import './Login.css';
+import { useEffect } from "react"
+import { connect } from "react-redux"; 
+import { setAuthedUser } from "../actions/authedUser";
+import { useNavigate } from "react-router-dom";
 
-const Login = ({ users }) => {
+const Login = ({ userIds, dispatch, authedUser }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // intended to redirect user if already logged in
+    // will not work when manually entering /login uri
+    // because it resets the redux store
+    if (authedUser !== null) {
+      navigate(`/`)
+    }
+  }, [])
+
+  const handleSelect = (e) => {
+    const userId = e.target.value
+    dispatch(setAuthedUser(userId))
+    navigate(`/`)
+  }
+
   return (
     <div className="login">
       <label htmlFor="userSelect">Select User ID:</label>
-      <select id="userSelect" name="userSelect">
-        {Object.values(users).map((user, index) => (
-          <option key={index} value={user.id}>
-            {user.id}
+      <select id="userSelect" name="userSelect" onChange={(e) => handleSelect(e)} value={"empty"}>
+        <option value="">
+          --Choose an option--
+        </option>
+        {userIds.map((id, index) => (
+          <option key={index} value={id}>
+            {id}
           </option>
         ))}
       </select>
@@ -16,4 +38,12 @@ const Login = ({ users }) => {
   );
 };
 
-export default Login;
+const mapStateToProps = ({ users, authedUser }) => {
+  const userIds = Object.values(users).map((user) => user.id)
+  return {
+    userIds,
+    authedUser: authedUser ? authedUser : null
+  }
+}
+
+export default connect(mapStateToProps)(Login);
