add45ea4367e2452090011f1cfaeb5f697903202
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-24 20:55:43 -0600

    fix: refactor
    add new components
    
    update routes in return statement
    
    add props from mapStateToStore tobe passed
    
    add dispatch to useEffect dependency array

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,46 @@
+import React from 'react';
 import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "../components/Dashboard";
 import LoadingBar from "react-redux-loading-bar";
-import QuestionPage from "../components/QuestionPage"
+import QuestionPage from "../components/QuestionPage";
 import NewQuestion from "../components/NewQuestion";
 import Leaderboard from "../components/Leaderboard";
 import Login from "../components/Login";
 import Nav from "../components/Nav";
-import RequireAuth from "../components/RequireAuth";
+import NotFound from "../components/NotFound";
 import { Routes, Route } from 'react-router-dom';
-import AnsweredQuestion from "./AnsweredQuestion";
-
+import RequireAuth from "./RequireAuth";
 const App = (props) => {
+  const { isAuthenticated, users, user, dispatch } = props;
+
   useEffect(() => {
-    props.dispatch(handleInitialData());
-  }, [])
+    dispatch(handleInitialData());
+  }, [dispatch]);
 
   return (
     <Fragment>
-      <Nav />
+      <Nav user={user}/>
       <LoadingBar />
       <Routes>
-        <Route path="login" element={<Login />} />
-
-        <Route element={<RequireAuth />}>
+        <Route path="/login" element={<Login />} />
+        <Route element={<RequireAuth isAuthenticated={isAuthenticated} />} >
           <Route path="/" exact element={<Dashboard />} />
-          <Route path="/question/:id" element={<QuestionPage />} />
+          <Route path="/questions/:qid" element={<QuestionPage />} />
           <Route path="/new" element={<NewQuestion />} />
-          <Route path="/answered/:id" element={<AnsweredQuestion />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/leaderboard" element={<Leaderboard users={users}/>} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </Fragment>
   );
 };
 
-const mapStateToProps = ({authedUser}) => ({
-  loading: authedUser === null,
-})
+const mapStateToProps = ({ authedUser, users }) => ({
+  isAuthenticated: authedUser !== null,
+  users: users ? users : null,
+  user: authedUser && users ? users[authedUser] : null,
+});
 
 export default connect(mapStateToProps)(App);
