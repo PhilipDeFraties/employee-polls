6cb8552bfb6e7a10aacd3ea256f755a43e1bc137
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-24 20:44:03 -0600

    feat: refactor
    updates composition of html
    
    adds logic to conditionally render user info
    
    removes mapStateToStore

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,51 +1,38 @@
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
 import { setAuthedUser } from "../actions/authedUser";
+import { connect } from "react-redux";
 
 const Nav = (props) => {
-  if (props.user === null) {
-    return (
-      <div class="topnav">
-        <h3 className="center">Please Log In</h3>
-      </div>
-    )
-  }
-
-  const { user } = props;
+  const { user, dispatch } = props;
 
   const handleClick = () => {
-    props.dispatch(setAuthedUser(null));
+    dispatch(setAuthedUser(null));
   }
 
   return (
-    <div class="topnav">
-      <a>
-        <Link to="/">Home</Link>
-      </a>
-      <a>
-        <Link to="/new">New Question</Link>
-      </a>
-      <a>
-        <Link to="/leaderboard">Leaderboard</Link>
-      </a>
-      <div class="topnav-right">
-        <b>
-          <img src={user.avatarURL} alt={`Avatar of ${user.id}`} className="avatar" />
-        </b>
-        <b>{user.id}</b>
-        <b>
-          <span onClick={handleClick}>Logout</span>
-        </b>
+    <div className="topnav" role="navigation">
+      <Link to="/">Home</Link>
+      <Link to="/new">New Question</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+      <div className="topnav-right">
+        {user ? (
+          <>
+            <b>
+              <img src={user.avatarURL} alt={`Avatar of ${user.id}`} className="avatar" />
+            </b>
+            <b>{user.id}</b>
+            <b>
+              <span onClick={handleClick}>Logout</span>
+            </b>
+          </>
+        ) : (
+          <b>
+            <Link to="/login">Login</Link>
+          </b>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = ({ authedUser, users }) => {
-  const user = users[authedUser]
-
-  return {
-    user: user ? user : null
-  }
-}
-export default connect(mapStateToProps)(Nav);
+export default connect()(Nav);
