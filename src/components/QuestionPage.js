b019f259fce13c13fd62b245239c81f94bd903cb
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-24 20:52:39 -0600

    fix: refactor
    sets authedUserVote state via useEffect
    
    handleClick updates state to rerender component
    
    vote buttons and poll data are shown depending on state
    
    update text displayed
    
    pass authedUserVote from mapStateToProps

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { handleSaveQuestionAnswer } from "../actions/questions";
 import { handleAddUserAnswer } from '../actions/users';
 import { useLocation, useNavigate, useParams } from "react-router-dom";
@@ -16,72 +16,93 @@ const withRouter = (Component) => {
 };
 
 const QuestionPage = (props) => {
-  const navigate = useNavigate();
+  const [authedUserVote, setauthedUserVote] = useState("");
 
   useEffect(() => {
-    if (props.question && props.userAnswered) {
-      navigate(`/answered/${props.question.id}`)
-    }
-  }, [])
-  
+    setauthedUserVote(props.authedUserVote);
+  }, [props.authedUserVote]);
+
   const handleClick = (option) => {
-    const { dispatch } = props
+    const { dispatch } = props;
     const questionAnswer = {
-      qid: id,
+      qid: props.router.params.qid,
       answer: option,
-    }
-    
-    dispatch(handleAddUserAnswer(questionAnswer))
-    dispatch(handleSaveQuestionAnswer(questionAnswer))
-    navigate(`/answered/${id}`);
-  }
-  
+    };
+
+    dispatch(handleAddUserAnswer(questionAnswer));
+    dispatch(handleSaveQuestionAnswer(questionAnswer));
+    setauthedUserVote(questionAnswer.answer);
+  };
+
   if (props.question === null) {
     return <h1>This Question doesn't exist</h1>;
   }
 
-  
-  const { question, users, authedUser } = props;
-  const { author, optionOne, optionTwo, id } = question;
+  const { question, users } = props;
+  const { author, optionOne, optionTwo } = question;
+
+  const totalVotes = optionOne.votes.length + optionTwo.votes.length;
+  const optionOnePercentage = ((optionOne.votes.length / totalVotes) * 100).toFixed(0);;
+  const optionTwoPercentage = ((optionTwo.votes.length / totalVotes) * 100).toFixed(0);;
 
   return (
-    <div>
+    <div className="question-page">
       <h1>Poll by {author}</h1>
       <img src={users[author].avatarURL} alt={`Avatar of ${author}`} className="avatar" />
-      <h3 className="center">Would You Rather:</h3>
-      <div className="center">
-        <div className="row">
-          <div className="column">
-            <div className="option-text">
-              {optionOne.text}
+      <h3>Would You Rather:</h3>
+      <div className="options-container">
+        <div className="option">
+          <div className="option-text">
+            {optionOne.text}
+          </div>
+          {authedUserVote ? (
+            <div className="votes">
+              Votes: {optionOne.votes.length} {`\n`} = {optionOnePercentage}%
             </div>
-            <button className="btn" onClick={() => handleClick("optionOne")} disabled={false}>
+            
+          ) : (
+            <button className="btn" onClick={() => handleClick("optionOne")}>
               Click
             </button>
+          )}
+          {authedUserVote === "optionOne" && (<div>You voted for this option</div>)}
+        </div>
+        <div className="option">
+          <div className="option-text">
+            {optionTwo.text}
           </div>
-          <div className="column">
-            <div className="option-text">
-              {optionTwo.text}
+          {authedUserVote ? (
+            <div className="votes">
+              Votes: {optionTwo.votes.length} {`\n`} = {optionTwoPercentage}%
             </div>
-            <button className="btn" onClick={() => handleClick("optionTwo")} disabled={false}>
+          ) : (
+            <button className="btn" onClick={() => handleClick("optionTwo")}>
               Click
             </button>
-          </div>
+          )}
+          {authedUserVote === "optionTwo" && (<div>You voted for this option</div>)}
         </div>
       </div>
     </div>
-  )
+  );
+};
 
-}
+const mapStateToProps = ({ questions, users, authedUser }, props) => {
+  const { qid } = props.router.params;
+  const question = questions[qid];
+  let authedUserVote = "";
 
-const mapStateToProps = ({questions, users, authedUser}, props) => {
-  const {id} = props.router.params;
-  const question = questions[id]
-  const userAnswered = Object.keys(users[authedUser].answers).includes(id)
+  if (question) {
+    if (question.optionOne.votes.includes(authedUser)) {
+      authedUserVote = "optionOne";
+    } else if (question.optionTwo.votes.includes(authedUser)) {
+      authedUserVote = "optionTwo";
+    }
+  }
   return {
     users,
-    userAnswered,
+    authedUserVote,
     question: question ? question : null,
-  }
-}
+  };
+};
 export default withRouter(connect(mapStateToProps)(QuestionPage));
